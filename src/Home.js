d08976f4ac1a6991a4eed0a75b1b113b34335506
Pavlin Ivanov <79521032+pavlin-js@users.noreply.github.com>
2021-04-19 20:57:28 +0300

    adding click events

--- src/Home.js
+++ src/Home.js
@@ -1,9 +1,22 @@
 const Home = () => {
+
+    let handleClick = ( e ) => {
+        console.log('hello ninja"s', e);
+    }
+
+    let handleClickAgain = (name, e) => {
+        console.log('hello ' + name, e.target.value);
+    }
+
     return (
         <div className='home'>
             <h2>Homepage</h2>
+            <button onClick={ handleClick } >Click Me</button>
+            <button onClick={ ( e ) => {
+                handleClickAgain('mario', e)
+            } } >Click Again</button>
         </div>
     );
 }
- 
+
 export default Home;
