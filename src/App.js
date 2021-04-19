1e777d83447a2d94a6c8585aa7527a4bf13c3f01
Pavlin Ivanov <79521032+pavlin-js@users.noreply.github.com>
2021-04-19 19:34:15 +0300

    dynamic values in templates

--- src/App.js
+++ src/App.js
@@ -2,12 +2,20 @@
 import './App.css';
 
 function App() {
+
+  let title = 'Welcome to the new block'
+  let likes = 50
+  let url = 'https://www.google.com/'
+
   return (
     <div className="App">
       <div className='content'>
         <h1>
-          App component
+          { title }
         </h1>
+        <p>Liked { likes } times</p>
+        <p>{ Math.floor(Math.random() * 10) }</p>
+        <a href={ url }>google</a>
       </div>
     </div>
   );
