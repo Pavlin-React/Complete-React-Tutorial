6ea7749ed647c64b84ccddcbe3ea02a7cad4bb0b
Pavlin Ivanov <79521032+pavlin-js@users.noreply.github.com>
2021-04-20 19:31:52 +0300

    Handling Fetch Error

--- src/Home.js
+++ src/Home.js
@@ -2,24 +2,38 @@ import { useState, useEffect } from "react";
 import BlogList from "./BlogList";
 
 const Home = () => {
-  let [blogs, setBlogs] = useState( null )
-  let [isPending, setIsPending] = useState( true )
+  let [blogs, setBlogs] = useState(null)
+  let [isPending, setIsPending] = useState(true)
+  let [error, setError] = useState(null)
 
+  useEffect(() => {
+    setTimeout(() => {
+      fetch('http://localhost:8000/blogs')
+        .then(res => {
+          if (!res.ok) {
+            throw Error('There is a Error')
+          }
+          return res.json()
+        })
+        .then(data => {
+          setBlogs(data)
+          setIsPending(false)
+          setError(null)
+        })
+        .catch(err => {
+          setIsPending(false)
+          setError(err.message)
 
-  useEffect( () => {
-    setTimeout( () => {
-      fetch( 'http://localhost:8000/blogs' )
-      .then( res => res.json() )
-      .then( data => setBlogs( data ) )
-      setIsPending( false )
-    }, 1000 )
-  }, [] )
+        })
+    }, 1000)
+  }, [])
 
 
   return (
     <div className='home'>
-      { isPending && <div>Loading...</div> }
-      { blogs && <BlogList blogs={ blogs } title='All blogs' /> }
+      { error && <div>{error}</div>}
+      { isPending && <div>Loading...</div>}
+      { blogs && <BlogList blogs={blogs} title='All blogs' />}
     </div>
   );
 }
