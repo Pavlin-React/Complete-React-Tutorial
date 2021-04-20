01ea0931ab30feb693db58b62e81094f899eca06
Pavlin Ivanov <79521032+pavlin-js@users.noreply.github.com>
2021-04-20 14:11:29 +0300

    functions as props

--- src/Home.js
+++ src/Home.js
@@ -7,10 +7,15 @@ const Home = () => {
     { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
     { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
   ] )
+
+  let handleDelete = ( id ) => {
+    let newBlogs = blogs.filter( blog => blog.id !== id )
+    setBlogs( newBlogs )
+  }
+
   return (
     <div className='home'>
-      <BlogList blogs={ blogs } title='All blogs' />
-      <BlogList blogs={ blogs.filter( ( blog ) => blog.author === 'mario' ) } title="Mario's blog" />
+      <BlogList blogs={ blogs } title='All blogs' handleDelete={ handleDelete } />
     </div>
   );
 }
