837cafa365ef51eea27a0f05155ff2bbfa27e477
Pavlin Ivanov <79521032+pavlin-js@users.noreply.github.com>
2021-04-20 19:04:34 +0300

    Conditional Loading message

--- src/Home.js
+++ src/Home.js
@@ -3,18 +3,22 @@ import BlogList from "./BlogList";
 
 const Home = () => {
   let [blogs, setBlogs] = useState( null )
-
+  let [isPending, setIsPending] = useState( true )
 
 
   useEffect( () => {
-    fetch( 'http://localhost:8000/blogs' )
+    setTimeout( () => {
+      fetch( 'http://localhost:8000/blogs' )
       .then( res => res.json() )
       .then( data => setBlogs( data ) )
+      setIsPending( false )
+    }, 1000 )
   }, [] )
 
 
   return (
     <div className='home'>
+      { isPending && <div>Loading...</div> }
       { blogs && <BlogList blogs={ blogs } title='All blogs' /> }
     </div>
   );
