75ac62f8de6c5a35e68b277efd2b2494a2cf969d
Pavlin Ivanov <79521032+pavlin-js@users.noreply.github.com>
2021-04-22 10:34:08 +0300

    Delete Fetch

--- src/BlogDetails.js
+++ src/BlogDetails.js
@@ -1,10 +1,19 @@
-import { useParams } from "react-router-dom";
+import { useHistory, useParams } from "react-router-dom";
 import useFetch from "./useFetch";
 
 const BlogDetails = () => {
 
   let { id } = useParams()
   let { data: blog, error, isPending } = useFetch('http://localhost:8000/blogs/' + id)
+  let history = useHistory()
+
+  let handleClick = () => {
+    fetch( 'http://localhost:8000/blogs/' + id, {
+      method: 'DELETE'
+    } ).then( () => {
+      history.push( '/' )
+    } )
+  }
 
   return (
     <div className='blog-details'>
@@ -15,6 +24,7 @@ const BlogDetails = () => {
           <h2>{ blog.title }</h2>
           <p>Written by { blog.author }</p>
           <div>{ blog.body }</div>
+          <button onClick={ handleClick } >Delete</button>
         </article>
       ) }
     </div>  
